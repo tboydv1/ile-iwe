0741c80ff453533a79c18ca0870e3497e04183c0
tboydv1 <tboydv1@gmail.com>
2021-11-08 10:59:04 +0100

    updates

--- src/main/java/com/ileiwe/security/user/LearningPartyServiceImpl.java
+++ src/main/java/com/ileiwe/security/user/LearningPartyServiceImpl.java
@@ -13,8 +13,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
